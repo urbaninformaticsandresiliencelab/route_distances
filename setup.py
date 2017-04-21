696c07e04450b44bac3ff5c7268cdef9856e8e8d
ercas <edgarrimandocastro@gmail.com>
2017-04-21 02:40:47 -0400

    changed version to 1.1.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 
 setuptools.setup(
     name = "route_distances",
-    version = "1.0.0",
+    version = "1.1.0",
     license = "MIT",
     description = "Classes for getting the distance of a route between two"
                   "places using various different services",
